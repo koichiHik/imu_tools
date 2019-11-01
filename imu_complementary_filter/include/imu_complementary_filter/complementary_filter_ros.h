eeb05b89dba74995b24c30072cc1f2b629b5a2ac
Koichi Hikawa <rkoichi2001@gmail.com>
2019-11-02 07:47:24 +0900

    Adoptation

--- imu_complementary_filter/include/imu_complementary_filter/complementary_filter_ros.h
+++ imu_complementary_filter/include/imu_complementary_filter/complementary_filter_ros.h
@@ -1,13 +1,14 @@
 /*
   @author Roberto G. Valenti <robertogl.valenti@gmail.com>
 
-	@section LICENSE
+        @section LICENSE
   Copyright (c) 2015, City University of New York
   CCNY Robotics Lab <http://robotics.ccny.cuny.edu>
-	All rights reserved.
+        All rights reserved.
 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
+        Redistribution and use in source and binary forms, with or without
+        modification, are permitted provided that the following conditions are
+  met:
      1. Redistributions of source code must retain the above copyright
         notice, this list of conditions and the following disclaimer.
      2. Redistributions in binary form must reproduce the above copyright
@@ -17,92 +18,99 @@
         names of its contributors may be used to endorse or promote products
         derived from this software without specific prior written permission.
 
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-	DISCLAIMED. IN NO EVENT SHALL the CCNY ROBOTICS LAB BE LIABLE FOR ANY
-	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+        DISCLAIMED. IN NO EVENT SHALL the CCNY ROBOTICS LAB BE LIABLE FOR ANY
+        DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #ifndef IMU_TOOLS_COMPLEMENTARY_FILTER_ROS_H
 #define IMU_TOOLS_COMPLEMENTARY_FILTER_ROS_H
 
-#include <sensor_msgs/MagneticField.h>
 #include <geometry_msgs/Vector3Stamped.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <message_filters/synchronizer.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
-#include <tf/transform_datatypes.h>
+#include <sensor_msgs/MagneticField.h>
 #include <tf/transform_broadcaster.h>
+#include <tf/transform_datatypes.h>
 
 #include "imu_complementary_filter/complementary_filter.h"
+#include "kmsgs/Imu.h"
 
 namespace imu_tools {
 
-class ComplementaryFilterROS
-{
-  public:
-    ComplementaryFilterROS(const ros::NodeHandle& nh, 
-                           const ros::NodeHandle& nh_private);    
-    virtual ~ComplementaryFilterROS();
+class ComplementaryFilterROS {
+public:
+  ComplementaryFilterROS(const ros::NodeHandle &nh,
+                         const ros::NodeHandle &nh_private);
+  virtual ~ComplementaryFilterROS();
 
-  private:
+private:
+  // Convenience typedefs
+  typedef sensor_msgs::Imu ImuMsg;
+  typedef sensor_msgs::MagneticField MagMsg;
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Imu,
+                                                          MagMsg>
+      MySyncPolicy;
+  typedef message_filters::sync_policies::ApproximateTime<ImuMsg, MagMsg>
+      SyncPolicy;
+  typedef message_filters::Synchronizer<SyncPolicy> Synchronizer;
+  typedef message_filters::Subscriber<ImuMsg> ImuSubscriber;
+  // typedef message_filters::Subscriber<kmsgs::Imu> ImuSubscriber;
+  typedef message_filters::Subscriber<MagMsg> MagSubscriber;
 
-    // Convenience typedefs
-    typedef sensor_msgs::Imu ImuMsg;
-    typedef sensor_msgs::MagneticField MagMsg;
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Imu, 
-        MagMsg> MySyncPolicy;
-    typedef message_filters::sync_policies::ApproximateTime<ImuMsg, MagMsg> 
-        SyncPolicy;
-    typedef message_filters::Synchronizer<SyncPolicy> Synchronizer;    
-    typedef message_filters::Subscriber<ImuMsg> ImuSubscriber; 
-    typedef message_filters::Subscriber<MagMsg> MagSubscriber;
+  // ROS-related variables.
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh_private_;
 
-    // ROS-related variables.
-    ros::NodeHandle nh_;
-    ros::NodeHandle nh_private_;
-    
-    boost::shared_ptr<Synchronizer> sync_;
-    boost::shared_ptr<ImuSubscriber> imu_subscriber_;
-    boost::shared_ptr<MagSubscriber> mag_subscriber_;
+  boost::shared_ptr<Synchronizer> sync_;
+  boost::shared_ptr<ImuSubscriber> imu_subscriber_;
+  boost::shared_ptr<MagSubscriber> mag_subscriber_;
 
-    ros::Publisher imu_publisher_;
-    ros::Publisher rpy_publisher_;
-    ros::Publisher state_publisher_;
-    tf::TransformBroadcaster tf_broadcaster_;
-         
-    // Parameters:
-    bool use_mag_;
-    bool publish_tf_;
-    bool reverse_tf_;
-    double constant_dt_;
-    bool publish_debug_topics_;
-    std::string fixed_frame_;
-    double orientation_variance_;
+  ros::Subscriber imu_raw_subscriber_;
+  ros::Publisher imu_publisher_;
+  ros::Publisher rpy_publisher_;
+  ros::Publisher state_publisher_;
+  tf::TransformBroadcaster tf_broadcaster_;
 
-    // State variables:
-    ComplementaryFilter filter_;
-    ros::Time time_prev_;
-    bool initialized_filter_;
+  // Parameters:
+  bool use_mag_;
+  bool publish_tf_;
+  bool reverse_tf_;
+  double constant_dt_;
+  bool publish_debug_topics_;
+  std::string fixed_frame_;
+  double orientation_variance_;
+  double acc_conv_ratio, ang_conv_ratio;
+  double offset_acc_x_, offset_acc_y_, offset_acc_z_;
+  double offset_gyro_x_, offset_gyro_y_, offset_gyro_z_;
 
-    void initializeParams();
-    void imuCallback(const ImuMsg::ConstPtr& imu_msg_raw);
-    void imuMagCallback(const ImuMsg::ConstPtr& imu_msg_raw,
-                        const MagMsg::ConstPtr& mav_msg);
-    void publish(const sensor_msgs::Imu::ConstPtr& imu_msg_raw);
+  // State variables:
+  ComplementaryFilter filter_;
+  ros::Time time_prev_;
+  bool initialized_filter_;
 
-    tf::Quaternion hamiltonToTFQuaternion(
-        double q0, double q1, double q2, double q3) const;
+  void initializeParams();
+  // void imuRawCallback(const kmsgs::Imu::ConstPtr &imu_msg_raw);
+  void imuRawCallback(const kmsgs::Imu &imu_msg_raw);
+  void imuCallback(const ImuMsg::ConstPtr &imu_msg_raw);
+  void imuMagCallback(const ImuMsg::ConstPtr &imu_msg_raw,
+                      const MagMsg::ConstPtr &mav_msg);
+  void publish(const sensor_msgs::Imu::ConstPtr &imu_msg_raw);
+
+  tf::Quaternion hamiltonToTFQuaternion(double q0, double q1, double q2,
+                                        double q3) const;
 };
 
-}  // namespace imu_tools
+} // namespace imu_tools
 
 #endif // IMU_TOOLS_COMPLEMENTARY_FILTER_ROS_H
--- imu_complementary_filter/src/complementary_filter_ros.cpp
+++ imu_complementary_filter/src/complementary_filter_ros.cpp
@@ -1,13 +1,14 @@
 /*
   @author Roberto G. Valenti <robertogl.valenti@gmail.com>
 
-	@section LICENSE
+        @section LICENSE
   Copyright (c) 2015, City University of New York
   CCNY Robotics Lab <http://robotics.ccny.cuny.edu>
-	All rights reserved.
+        All rights reserved.
 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
+        Redistribution and use in source and binary forms, with or without
+        modification, are permitted provided that the following conditions are
+  met:
      1. Redistributions of source code must retain the above copyright
         notice, this list of conditions and the following disclaimer.
      2. Redistributions in binary form must reproduce the above copyright
@@ -17,149 +18,188 @@
         names of its contributors may be used to endorse or promote products
         derived from this software without specific prior written permission.
 
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-	DISCLAIMED. IN NO EVENT SHALL the CCNY ROBOTICS LAB BE LIABLE FOR ANY
-	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+        DISCLAIMED. IN NO EVENT SHALL the CCNY ROBOTICS LAB BE LIABLE FOR ANY
+        DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+//
 #include "imu_complementary_filter/complementary_filter_ros.h"
 
-#include <std_msgs/Float64.h>
+// ROS
 #include <std_msgs/Bool.h>
+#include <std_msgs/Float64.h>
 
 namespace imu_tools {
 
+void ReadParam(const std::string &param_name, bool result) {
+  if (!result) {
+    ROS_FATAL("Param read failure. : %s", param_name.c_str());
+  }
+}
+
 ComplementaryFilterROS::ComplementaryFilterROS(
-    const ros::NodeHandle& nh,
-    const ros::NodeHandle& nh_private):
-  nh_(nh),
-  nh_private_(nh_private),
-  initialized_filter_(false)
-{
+    const ros::NodeHandle &nh, const ros::NodeHandle &nh_private)
+    : nh_(nh), nh_private_(nh_private), initialized_filter_(false) {
   ROS_INFO("Starting ComplementaryFilterROS");
   initializeParams();
 
   int queue_size = 5;
 
   // Register publishers:
-  imu_publisher_ = nh_.advertise<sensor_msgs::Imu>(ros::names::resolve("imu") + "/data", queue_size);
+  imu_publisher_ = nh_.advertise<sensor_msgs::Imu>(
+      ros::names::resolve("imu") + "/data", queue_size);
 
-  if (publish_debug_topics_)
-  {
-      rpy_publisher_ = nh_.advertise<geometry_msgs::Vector3Stamped>(
-                  ros::names::resolve("imu") + "/rpy/filtered", queue_size);
+  if (publish_debug_topics_) {
+    rpy_publisher_ = nh_.advertise<geometry_msgs::Vector3Stamped>(
+        ros::names::resolve("imu") + "/rpy/filtered", queue_size);
 
-      if (filter_.getDoBiasEstimation())
-      {
-        state_publisher_ = nh_.advertise<std_msgs::Bool>(
-                    ros::names::resolve("imu") + "/steady_state", queue_size);
-      }
+    if (filter_.getDoBiasEstimation()) {
+      state_publisher_ = nh_.advertise<std_msgs::Bool>(
+          ros::names::resolve("imu") + "/steady_state", queue_size);
+    }
   }
 
+  /**
   // Register IMU raw data subscriber.
-  imu_subscriber_.reset(new ImuSubscriber(nh_, ros::names::resolve("imu") + "/data_raw", queue_size));
+  imu_subscriber_.reset(new ImuSubscriber(
+      nh_, ros::names::resolve("imu") + "/data_raw", queue_size));
 
   // Register magnetic data subscriber.
-  if (use_mag_)
-  {
-    mag_subscriber_.reset(new MagSubscriber(nh_, ros::names::resolve("imu") + "/mag", queue_size));
+  if (use_mag_) {
+    mag_subscriber_.reset(new MagSubscriber(
+        nh_, ros::names::resolve("imu") + "/mag", queue_size));
 
-    sync_.reset(new Synchronizer(
-        SyncPolicy(queue_size), *imu_subscriber_, *mag_subscriber_));
+    sync_.reset(new Synchronizer(SyncPolicy(queue_size), *imu_subscriber_,
+                                 *mag_subscriber_));
     sync_->registerCallback(
         boost::bind(&ComplementaryFilterROS::imuMagCallback, this, _1, _2));
+  } else {
+    // imu_subscriber_->registerCallback(&ComplementaryFilterROS::imuCallback,
+    //                                  this);
+    // imu_raw_subscriber->registerCallback(
+    //    &ComplementaryFilterROS::imuRawCallback, this);
+    // imu_raw_subscriber.subscribe(&ComplementaryFilterROS::imuRawCallback,
+    // this);
   }
-  else
-  {
-    imu_subscriber_->registerCallback(
-        &ComplementaryFilterROS::imuCallback, this);
-  }
+  **/
+
+  ROS_INFO("Before Subscribe");
+  imu_raw_subscriber_ =
+      nh_.subscribe("/imu/data_raw", queue_size,
+                    &ComplementaryFilterROS::imuRawCallback, this);
+  ROS_INFO("After Subscribe");
 }
 
-ComplementaryFilterROS::~ComplementaryFilterROS()
-{
+ComplementaryFilterROS::~ComplementaryFilterROS() {
   ROS_INFO("Destroying ComplementaryFilterROS");
 }
 
-void ComplementaryFilterROS::initializeParams()
-{
+void ComplementaryFilterROS::initializeParams() {
   double gain_acc;
   double gain_mag;
   bool do_bias_estimation;
   double bias_alpha;
   bool do_adaptive_gain;
 
-  if (!nh_private_.getParam ("fixed_frame", fixed_frame_))
-    fixed_frame_ = "odom";
-  if (!nh_private_.getParam ("use_mag", use_mag_))
-    use_mag_ = false;
-  if (!nh_private_.getParam ("publish_tf", publish_tf_))
-    publish_tf_ = false;
-  if (!nh_private_.getParam ("reverse_tf", reverse_tf_))
-    reverse_tf_ = false;
-  if (!nh_private_.getParam ("constant_dt", constant_dt_))
-    constant_dt_ = 0.0;
-  if (!nh_private_.getParam ("publish_debug_topics", publish_debug_topics_))
-    publish_debug_topics_ = false;
-  if (!nh_private_.getParam ("gain_acc", gain_acc))
-    gain_acc = 0.01;
-  if (!nh_private_.getParam ("gain_mag", gain_mag))
-    gain_mag = 0.01;
-  if (!nh_private_.getParam ("do_bias_estimation", do_bias_estimation))
-    do_bias_estimation = true;
-  if (!nh_private_.getParam ("bias_alpha", bias_alpha))
-    bias_alpha = 0.01;
-  if (!nh_private_.getParam ("do_adaptive_gain", do_adaptive_gain))
-    do_adaptive_gain = true;
+  // Frame
+  ReadParam("fixed_frame", nh_private_.param<std::string>(
+                               "fixed_frame", fixed_frame_, "odom"));
+  ReadParam("usg_mag", nh_private_.param<bool>("use_mag", use_mag_, false));
 
-  double orientation_stddev;
-  if (!nh_private_.getParam ("orientation_stddev", orientation_stddev))
-    orientation_stddev = 0.0;
+  // TF Configuration.
+  ReadParam("publish_tf",
+            nh_private_.param<bool>("publish_tf", publish_tf_, false));
+  ReadParam("reverse_tf",
+            nh_private_.param<bool>("reverse_tf", reverse_tf_, false));
+  ReadParam("constant_dt",
+            nh_private_.param<double>("constant_dt", constant_dt_, 0.0));
 
-  orientation_variance_ = orientation_stddev * orientation_stddev;
+  // Conversion
+  ReadParam("acc_conv_ratio",
+            nh_private_.param<double>("acc_conv_ratio", acc_conv_ratio, 0.0));
+  ReadParam("ang_conv_ratio",
+            nh_private_.param<double>("ang_conv_ratio", ang_conv_ratio, 0.0));
+
+  // Offset Setting
+  ReadParam("offset_acc_x",
+            nh_private_.param<double>("offset_acc_x", offset_acc_x_, 0.0));
+  ReadParam("offset_acc_y",
+            nh_private_.param<double>("offset_acc_y", offset_acc_y_, 0.0));
+  ReadParam("offset_acc_z",
+            nh_private_.param<double>("offset_acc_z", offset_acc_z_, 0.0));
+  ReadParam("offset_gyro_x",
+            nh_private_.param<double>("offset_gyro_x", offset_gyro_x_, 0.0));
+  ReadParam("offset_gyro_y",
+            nh_private_.param<double>("offset_gyro_y", offset_gyro_y_, 0.0));
+  ReadParam("offset_gyro_z",
+            nh_private_.param<double>("offset_gyro_z", offset_gyro_z_, 0.0));
+
+  // Gain Setting.
+  ReadParam("gain_acc", nh_private_.param<double>("gain_acc", gain_acc, 0.01));
+  ReadParam("gain_mag", nh_private_.param<double>("gain_mag", gain_mag, 0.01));
+  ReadParam(
+      "do_adaptive_gain",
+      nh_private_.param<bool>("do_adaptive_gain", do_adaptive_gain, true));
+
+  // Bias Setting.
+  ReadParam(
+      "do_bias_estimation",
+      nh_private_.param<bool>("do_bias_estimation", do_bias_estimation, true));
+  ReadParam("bias_alpha",
+            nh_private_.param<double>("bias_alpha", bias_alpha, 0.01));
+
+  {
+    double orientation_stddev;
+    ReadParam("orientation_stddev",
+              nh_private_.param<double>("orientation_stddev",
+                                        orientation_stddev, 0.0));
+    orientation_variance_ = orientation_stddev * orientation_stddev;
+  }
+
+  // Debug Related.
+  ReadParam("publish_debug_topics",
+            nh_private_.param<bool>("publish_debug_topics",
+                                    publish_debug_topics_, false));
 
   filter_.setDoBiasEstimation(do_bias_estimation);
   filter_.setDoAdaptiveGain(do_adaptive_gain);
 
-  if(!filter_.setGainAcc(gain_acc))
+  if (!filter_.setGainAcc(gain_acc))
     ROS_WARN("Invalid gain_acc passed to ComplementaryFilter.");
-  if (use_mag_)
-  {
-    if(!filter_.setGainMag(gain_mag))
+  if (use_mag_) {
+    if (!filter_.setGainMag(gain_mag))
       ROS_WARN("Invalid gain_mag passed to ComplementaryFilter.");
   }
-  if (do_bias_estimation)
-  {
-    if(!filter_.setBiasAlpha(bias_alpha))
+  if (do_bias_estimation) {
+    if (!filter_.setBiasAlpha(bias_alpha))
       ROS_WARN("Invalid bias_alpha passed to ComplementaryFilter.");
   }
 
   // check for illegal constant_dt values
-  if (constant_dt_ < 0.0)
-  {
+  if (constant_dt_ < 0.0) {
     // if constant_dt_ is 0.0 (default), use IMU timestamp to determine dt
     // otherwise, it will be constant
-    ROS_WARN("constant_dt parameter is %f, must be >= 0.0. Setting to 0.0", constant_dt_);
+    ROS_WARN("constant_dt parameter is %f, must be >= 0.0. Setting to 0.0",
+             constant_dt_);
     constant_dt_ = 0.0;
   }
 }
 
-void ComplementaryFilterROS::imuCallback(const ImuMsg::ConstPtr& imu_msg_raw)
-{
-  const geometry_msgs::Vector3& a = imu_msg_raw->linear_acceleration;
-  const geometry_msgs::Vector3& w = imu_msg_raw->angular_velocity;
-  const ros::Time& time = imu_msg_raw->header.stamp;
+void ComplementaryFilterROS::imuCallback(const ImuMsg::ConstPtr &imu_msg_raw) {
+  const geometry_msgs::Vector3 &a = imu_msg_raw->linear_acceleration;
+  const geometry_msgs::Vector3 &w = imu_msg_raw->angular_velocity;
+  const ros::Time &time = imu_msg_raw->header.stamp;
 
   // Initialize.
-  if (!initialized_filter_)
-  {
+  if (!initialized_filter_) {
     time_prev_ = time;
     initialized_filter_ = true;
     return;
@@ -167,10 +207,11 @@ void ComplementaryFilterROS::imuCallback(const ImuMsg::ConstPtr& imu_msg_raw)
 
   // determine dt: either constant, or from IMU timestamp
   double dt;
-  if (constant_dt_ > 0.0)
+  if (constant_dt_ > 0.0) {
     dt = constant_dt_;
-  else
+  } else {
     dt = (time - time_prev_).toSec();
+  }
 
   time_prev_ = time;
 
@@ -181,17 +222,69 @@ void ComplementaryFilterROS::imuCallback(const ImuMsg::ConstPtr& imu_msg_raw)
   publish(imu_msg_raw);
 }
 
-void ComplementaryFilterROS::imuMagCallback(const ImuMsg::ConstPtr& imu_msg_raw,
-                                            const MagMsg::ConstPtr& mag_msg)
-{
-  const geometry_msgs::Vector3& a = imu_msg_raw->linear_acceleration;
-  const geometry_msgs::Vector3& w = imu_msg_raw->angular_velocity;
-  const geometry_msgs::Vector3& m = mag_msg->magnetic_field;
-  const ros::Time& time = imu_msg_raw->header.stamp;
+void ComplementaryFilterROS::imuRawCallback(
+    //    const kmsgs::Imu::ConstPtr &imu_msg_raw) {
+    const kmsgs::Imu &imu_msg_raw) {
+
+  ROS_ERROR("Inside imuRawCallback");
+
+  boost::shared_ptr<sensor_msgs::Imu> imu_msg =
+      boost::make_shared<sensor_msgs::Imu>();
+
+  imu_msg->header = imu_msg_raw.header;
+  ros::Time time = imu_msg_raw.header.stamp;
+
+  // Linear Acceleration.
+  imu_msg->linear_acceleration.x =
+      acc_conv_ratio * (imu_msg_raw.accX + offset_acc_x_);
+  imu_msg->linear_acceleration.y =
+      acc_conv_ratio * (imu_msg_raw.accY + offset_acc_y_);
+  imu_msg->linear_acceleration.z =
+      acc_conv_ratio * (imu_msg_raw.accZ + offset_acc_z_);
+
+  // Angular Velocity.
+  imu_msg->angular_velocity.x =
+      ang_conv_ratio * (imu_msg_raw.omegaX + offset_gyro_x_);
+  imu_msg->angular_velocity.y =
+      ang_conv_ratio * (imu_msg_raw.omegaY + offset_gyro_y_);
+  imu_msg->angular_velocity.z =
+      ang_conv_ratio * (imu_msg_raw.omegaZ + offset_gyro_z_);
 
   // Initialize.
-  if (!initialized_filter_)
-  {
+  if (!initialized_filter_) {
+    time_prev_ = time;
+    initialized_filter_ = true;
+    return;
+  }
+
+  // determine dt: either constant, or from IMU timestamp
+  double dt;
+  if (constant_dt_ > 0.0) {
+    dt = constant_dt_;
+  } else {
+    dt = (time - time_prev_).toSec();
+  }
+
+  time_prev_ = time;
+
+  filter_.update(imu_msg->linear_acceleration.x, imu_msg->linear_acceleration.y,
+                 imu_msg->linear_acceleration.z, imu_msg->angular_velocity.x,
+                 imu_msg->angular_velocity.y, imu_msg->angular_velocity.z, dt);
+
+  publish(imu_msg);
+  // publish(ImuMsg::ConstPtr(imu_msg));
+}
+
+void ComplementaryFilterROS::imuMagCallback(const ImuMsg::ConstPtr &imu_msg_raw,
+                                            const MagMsg::ConstPtr &mag_msg) {
+
+  const geometry_msgs::Vector3 &a = imu_msg_raw->linear_acceleration;
+  const geometry_msgs::Vector3 &w = imu_msg_raw->angular_velocity;
+  const geometry_msgs::Vector3 &m = mag_msg->magnetic_field;
+  const ros::Time &time = imu_msg_raw->header.stamp;
+
+  // Initialize.
+  if (!initialized_filter_) {
     time_prev_ = time;
     initialized_filter_ = true;
     return;
@@ -200,32 +293,33 @@ void ComplementaryFilterROS::imuMagCallback(const ImuMsg::ConstPtr& imu_msg_raw,
   // Calculate dt.
   double dt = (time - time_prev_).toSec();
   time_prev_ = time;
-   //ros::Time t_in, t_out;
-  //t_in = ros::Time::now();
+  // ros::Time t_in, t_out;
+  // t_in = ros::Time::now();
   // Update the filter.
-  if (std::isnan(m.x) || std::isnan(m.y) || std::isnan(m.z))
+  if (std::isnan(m.x) || std::isnan(m.y) || std::isnan(m.z)) {
     filter_.update(a.x, a.y, a.z, w.x, w.y, w.z, dt);
-  else
+  } else {
     filter_.update(a.x, a.y, a.z, w.x, w.y, w.z, m.x, m.y, m.z, dt);
+  }
 
-  //t_out = ros::Time::now();
-  //float dt_tot = (t_out - t_in).toSec() * 1000.0; // In msec.
-  //printf("%.6f\n", dt_tot);
+  // t_out = ros::Time::now();
+  // float dt_tot = (t_out - t_in).toSec() * 1000.0; // In msec.
+  // printf("%.6f\n", dt_tot);
   // Publish state.
   publish(imu_msg_raw);
 }
 
-tf::Quaternion ComplementaryFilterROS::hamiltonToTFQuaternion(
-    double q0, double q1, double q2, double q3) const
-{
+tf::Quaternion ComplementaryFilterROS::hamiltonToTFQuaternion(double q0,
+                                                              double q1,
+                                                              double q2,
+                                                              double q3) const {
   // ROS uses the Hamilton quaternion convention (q0 is the scalar). However,
   // the ROS quaternion is in the form [x, y, z, w], with w as the scalar.
   return tf::Quaternion(q1, q2, q3, q0);
 }
 
 void ComplementaryFilterROS::publish(
-    const sensor_msgs::Imu::ConstPtr& imu_msg_raw)
-{
+    const sensor_msgs::Imu::ConstPtr &imu_msg_raw) {
   // Get the orientation:
   double q0, q1, q2, q3;
   filter_.getOrientation(q0, q1, q2, q3);
@@ -247,8 +341,7 @@ void ComplementaryFilterROS::publish(
   imu_msg->orientation_covariance[8] = orientation_variance_;
 
   // Account for biases.
-  if (filter_.getDoBiasEstimation())
-  {
+  if (filter_.getDoBiasEstimation()) {
     imu_msg->angular_velocity.x -= filter_.getAngularVelocityBiasX();
     imu_msg->angular_velocity.y -= filter_.getAngularVelocityBiasY();
     imu_msg->angular_velocity.z -= filter_.getAngularVelocityBiasZ();
@@ -256,50 +349,40 @@ void ComplementaryFilterROS::publish(
 
   imu_publisher_.publish(imu_msg);
 
-  if (publish_debug_topics_)
-  {
-      // Create and publish roll, pitch, yaw angles
-      geometry_msgs::Vector3Stamped rpy;
-      rpy.header = imu_msg_raw->header;
+  if (publish_debug_topics_) {
+    // Create and publish roll, pitch, yaw angles
+    geometry_msgs::Vector3Stamped rpy;
+    rpy.header = imu_msg_raw->header;
 
-      tf::Matrix3x3 M;
-      M.setRotation(q);
-      M.getRPY(rpy.vector.x, rpy.vector.y, rpy.vector.z);
-      rpy_publisher_.publish(rpy);
+    tf::Matrix3x3 M;
+    M.setRotation(q);
+    M.getRPY(rpy.vector.x, rpy.vector.y, rpy.vector.z);
+    rpy_publisher_.publish(rpy);
 
-      // Publish whether we are in the steady state, when doing bias estimation
-      if (filter_.getDoBiasEstimation())
-      {
-        std_msgs::Bool state_msg;
-        state_msg.data = filter_.getSteadyState();
-        state_publisher_.publish(state_msg);
-      }
+    // Publish whether we are in the steady state, when doing bias estimation
+    if (filter_.getDoBiasEstimation()) {
+      std_msgs::Bool state_msg;
+      state_msg.data = filter_.getSteadyState();
+      state_publisher_.publish(state_msg);
+    }
   }
 
-  if (publish_tf_)
-  {
-      // Create and publish the ROS tf.
-      tf::Transform transform;
-      transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
-      transform.setRotation(q);
+  if (publish_tf_) {
+    // Create and publish the ROS tf.
+    tf::Transform transform;
+    transform.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
+    transform.setRotation(q);
 
-      if (reverse_tf_)
-      {
-          tf_broadcaster_.sendTransform(
-              tf::StampedTransform(transform.inverse(),
-                                   imu_msg_raw->header.stamp,
-                                   imu_msg_raw->header.frame_id,
-                                   fixed_frame_));
-      }
-      else
-      {
-          tf_broadcaster_.sendTransform(
-              tf::StampedTransform(transform,
-                                   imu_msg_raw->header.stamp,
-                                   fixed_frame_,
-                                   imu_msg_raw->header.frame_id));
-      }
+    if (reverse_tf_) {
+      tf_broadcaster_.sendTransform(
+          tf::StampedTransform(transform.inverse(), imu_msg_raw->header.stamp,
+                               imu_msg_raw->header.frame_id, fixed_frame_));
+    } else {
+      tf_broadcaster_.sendTransform(
+          tf::StampedTransform(transform, imu_msg_raw->header.stamp,
+                               fixed_frame_, imu_msg_raw->header.frame_id));
+    }
   }
 }
 
-}  // namespace imu_tools
+} // namespace imu_tools
